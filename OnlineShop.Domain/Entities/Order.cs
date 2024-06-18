6851607b675020154ac29c86227fb4af6e498dfa
UniTi <sasha.sukavaty@gmail.com>
2024-06-19 01:06:48 +0300

    Реквестик фор Арсен

---
+++ OnlineShop.Domain/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShop.Domain.Entities;
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+
