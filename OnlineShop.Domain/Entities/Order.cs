969807b75971b5fa043a193f3f278940440026f5
UniTi <sasha.sukavaty@gmail.com>
2024-06-19 01:49:39 +0300

    Сущности и бдконтекст

--- OnlineShop.Domain/Entities/Order.cs
+++ OnlineShop.Domain/Entities/Order.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace OnlineShop.Domain.Entities;
 
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string UserId { get; set; }
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string UserId { get; set; }
 
-        public User User { get; set; }
-        public ICollection<OrderItem> OrderItems { get; set; }
-    }
+    public User User { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
+}
 
--- OnlineShop.Domain/Entities/Product.cs
+++ OnlineShop.Domain/Entities/Product.cs
@@ -2,15 +2,15 @@
 
 namespace OnlineShop.Domain.Entities;
 
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
-        public decimal ProductPrice { get; set; }
-        public int CategoryId { get; set; }
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+    public int CategoryId { get; set; }
 
-        public Category Category { get; set; }
-        public ICollection<OrderItem> OrderItems { get; set; }
-    }
+    public Category Category { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
+}
 
