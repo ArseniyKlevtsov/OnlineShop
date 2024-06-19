09f554e6930b4f1628088ae6188e9b5884f2a324
arseniy <arseniyklevts@gmail.com>
2024-06-19 15:22:31 +0300

    make all methods in BaseRepository virtual

--- OnlineShop.Infrastructure/Repositories/BaseRepository.cs
+++ OnlineShop.Infrastructure/Repositories/BaseRepository.cs
@@ -14,28 +14,28 @@ public class BaseRepository<TEntity, Tkey> : IBaseRepository<TEntity, Tkey>
         _context = context;
         _dbSet = context.Set<TEntity>();
     }
-    public async Task<TEntity?> Get(Tkey key)
+    public virtual async Task<TEntity?> Get(Tkey key)
     {
         return await _dbSet.FindAsync(key);
     }
 
-    public async Task<IEnumerable<TEntity>> GetAll()
+    public virtual async Task<IEnumerable<TEntity>> GetAll()
     {
         return await _dbSet.ToListAsync();
     }
 
-    public async void Add(TEntity entity)
+    public virtual async void Add(TEntity entity)
     {
         await _dbSet.AddAsync(entity);
         await _context.SaveChangesAsync();
     }
-    public async void Update(TEntity entity)
+    public virtual async void Update(TEntity entity)
     {
         _context.Entry(entity).State = EntityState.Modified;
         await _context.SaveChangesAsync();
     }
 
-    public async void Delete(TEntity entity)
+    public virtual async void Delete(TEntity entity)
     {
         _dbSet.Remove(entity);
         await _context.SaveChangesAsync();
