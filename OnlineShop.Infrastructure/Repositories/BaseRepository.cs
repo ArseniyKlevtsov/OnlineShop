cf9b2fc774d8b258581b4b205dd78fa69ff4a524
ArseniyKlevtsov <127730797+ArseniyKlevtsov@users.noreply.github.com>
2024-06-21 11:34:55 +0300

    Add IBaseRepository and its implementation (#4)
    * Add IBaseRepository and its implementation"
    
    * make all methods in BaseRepository virtual
    
    * rename interface : IBaseRepository to IRepository. set protected to DbContext and DbSet.
    
    * fix
    
    * change DbContext to ApplicationDbContext
    
    * change name -> GetByIdAsync in BaseRepos
    
    * delete unnecessary using

---
+++ OnlineShop.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Domain;
+using OnlineShop.Domain.Interfaces;
+
+
+namespace OnlineShop.Infrastructure.Repositories;
+
+public class BaseRepository<TEntity> : IRepository<TEntity> 
+    where TEntity : class
+{
+    protected ApplicationDbContext _context;
+    protected DbSet<TEntity> _dbSet;
+
+    public BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+    public virtual async Task<TEntity?> GetByIdAsync(int id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+
+        if (entity != null)
+        {
+            _context.Entry(entity).State = EntityState.Detached;
+        }
+        return entity;
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public virtual async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+    public virtual async Task UpdateAsync(TEntity entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+}
