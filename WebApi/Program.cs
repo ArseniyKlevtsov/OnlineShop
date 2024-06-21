44a4063bde4ea206d476ff2eedef3d69acbc1e61
Даша Рум <dasharum03@mail.ru>
2024-06-21 12:00:48 +0300

    feature/OrderRepository

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Domain;
+using OnlineShop.Domain.Interfaces;
+using OnlineShop.Infrastructure.Repositories;
 
 namespace WebApi
 {
@@ -8,11 +12,15 @@ namespace WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Register ApplicationDbContext and repositories
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
             var app = builder.Build();
 
@@ -24,9 +32,7 @@ namespace WebApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
 
             app.MapControllers();
 
