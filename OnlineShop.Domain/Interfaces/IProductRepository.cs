1fd821845a813fae6c49822f45cf4b0539b011c7
arseniy <arseniyklevts@gmail.com>
2024-06-21 10:54:06 +0300

    add ProductRepository and its interface

---
+++ OnlineShop.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Domain.Interfaces;
+
+public interface IProductRepository: IRepository<Product>
+{
+    // filters
+    Task<IEnumerable<Product>> GetByCategoryAsync(Category category);
+    Task<IEnumerable<Product>> GetByCategoryIdAsync(int categoryId);
+
+    // includes tables
+    Task<IEnumerable<Product>> GetIncludeAllAsync();
+    Task<IEnumerable<Product>> GetIncludeCategoryAsync();
+    Task<IEnumerable<Product>> GetIncludeOrderItemsAsync();
+
+    // filters + includes
+    Task<IEnumerable<Product>> GetByCategoryIncludeAllAsync(Category category);
+    Task<IEnumerable<Product>> GetByCategoryIdIncludeAllAsync(int categoryId);
+
+}
