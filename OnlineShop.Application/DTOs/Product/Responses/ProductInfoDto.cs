a49538d10d61e18c0e73f553024bb1b9b535dc6b
ArseniyKlevtsov <127730797+ArseniyKlevtsov@users.noreply.github.com>
2024-06-23 21:07:46 +0300

    Feature/i product service+dt os+validation (#21)
    * add product service interface DTOs and FluentValidation
    
    * dividing the DTO into a response and a request. rename iProductService -> IProductService
    
    * add messages in ProductValidator

---
+++ OnlineShop.Application/DTOs/Product/Responses/ProductInfoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineShop.Application.DTOs.Product.Responses;
+
+public class ProductInfoDto
+{
+    public string? ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+}
