4a8b3f2ca18b60019bc8c426ab66c3e9858dca4a
John-Gavr <johngavr19@gmail.com>
2024-06-23 20:56:58 +0300

    Created order service interface, DTOs and added FluentValidation
    Created order service interface, DTOs and added FluentValidation

---
+++ OnlineShop.Application/DTOs/Order/Responses/GetOrderWithDetailsResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Application.DTOs.Order.Responses;
+
+public class GetOrderWithDetailsResponseDto
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string UserId { get; set; }
+    public UserResponseDto User { get; set; }
+    public ICollection<OrderItemDtoResponse> OrderItems { get; set; }
+}
