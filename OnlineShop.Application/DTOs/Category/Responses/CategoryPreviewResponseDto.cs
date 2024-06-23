77eabdbb3b5adf55a5067fa88c94d8f718840315
Даша Рум <dasharum03@mail.ru>
2024-06-23 14:11:28 +0300

    feature/ICategoryService+DTO+FluentValidation

---
+++ OnlineShop.Application/DTOs/Category/Responses/CategoryPreviewResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Application.DTOs.Category.Responses;
+
+public class CategoryPreviewResponseDto
+{
+    public string? CategoryName { get; set; }
+    public ICollection<Product>? Products { get; set; }
+}
