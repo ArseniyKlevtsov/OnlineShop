82b80a999833e449804779aafe6a3eb6c66d6b15
arseniy <arseniyklevts@gmail.com>
2024-06-23 22:25:46 +0300

    user service interface dtos and FV

---
+++ OnlineShop.Application/DTOs/UserDTOs/Responses/UserWithRolesResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Application.DTOs.UserDTOs.Responses;
+public class UserWithRolesResponse
+{
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public ICollection<string>? Roles { get; set; }
+}
---
+++ OnlineShop.Application/FluentValidation/UserValidator .cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Application.FluentValidation;
+
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(u => u.Id)
+            .NotEmpty()
+            .WithMessage("User ID cannot be empty.");
+
+        RuleFor(u => u.UserName)
+            .NotEmpty()
+            .WithMessage("Username cannot be empty.")
+            .MaximumLength(50)
+            .WithMessage("Username cannot be longer than 50 characters.");
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .WithMessage("Email cannot be empty.")
+            .EmailAddress()
+            .WithMessage("Invalid email address.");
+
+        RuleFor(u => u.FirstName)
+            .NotEmpty()
+            .WithMessage("First name cannot be empty.")
+            .MaximumLength(50)
+            .WithMessage("First name cannot be longer than 50 characters.");
+
+        RuleFor(u => u.LastName)
+            .NotEmpty()
+            .WithMessage("Last name cannot be empty.")
+            .MaximumLength(50)
+            .WithMessage("Last name cannot be longer than 50 characters.");
+
+        RuleFor(u => u.PasswordHash)
+            .NotEmpty()
+            .WithMessage("Password hash cannot be empty.");
+
+        RuleFor(u => u.SecurityStamp)
+            .NotEmpty()
+            .WithMessage("Security stamp cannot be empty.");
+
+        RuleFor(u => u.EmailConfirmed)
+            .NotNull()
+            .WithMessage("Email confirmation status cannot be null.");
+
+        RuleFor(u => u.PhoneNumberConfirmed)
+            .NotNull()
+            .WithMessage("Phone number confirmation status cannot be null.");
+
+        RuleFor(u => u.AccessFailedCount)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Access failed count cannot be negative.");
+    }
+}
