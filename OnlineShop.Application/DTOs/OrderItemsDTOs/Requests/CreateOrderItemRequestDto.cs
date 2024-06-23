bfd03c556d517faa53c9881644fc9dfff4b854ab
arseniy <arseniyklevts@gmail.com>
2024-06-23 22:42:04 +0300

    OrderItem DTO+IService+FV

---
+++ OnlineShop.Application/DTOs/OrderItemsDTOs/Requests/CreateOrderItemRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Application.DTOs.OrderItemsDTOs.Requests;
+public class CreateOrderItemRequestDto
+{
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
---
+++ OnlineShop.Application/FluentValidation/OrderItemValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Application.FluentValidation;
+
+public class OrderItemValidator : AbstractValidator<OrderItem>
+{
+    public OrderItemValidator()
+    {
+        RuleFor(x => x.OrderId)
+            .GreaterThan(0)
+            .WithMessage("OrderId must be greater than 0");
+
+        RuleFor(x => x.ProductId)
+            .GreaterThan(0)
+            .WithMessage("ProductId must be greater than 0");
+
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0)
+            .WithMessage("Quantity must be greater than 0");
+
+        RuleFor(x => x.UnitPrice)
+            .GreaterThanOrEqualTo(0m)
+            .WithMessage("UnitPrice must be greater than or equal to 0");
+    }
+}
