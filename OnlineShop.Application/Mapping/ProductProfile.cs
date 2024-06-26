18e190972de4120bd1873d29d35db15ddbae57b0
arseniy <arseniyklevts@gmail.com>
2024-06-27 01:48:32 +0300

    add mapper for product

---
+++ OnlineShop.Application/Mapping/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OnlineShop.Application.DTOs.ProductDTOs.Requests;
+using OnlineShop.Application.DTOs.ProductDTOs.Responses;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Application.Mapping;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductRequestDto>().ReverseMap();
+
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<Product, ProductInfoDto>().ReverseMap();
+        CreateMap<Product, ProductPreViewDto>().ReverseMap();
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -14,7 +14,7 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
             // Add Infrastructure services
             builder.Services.AddInfrastructure(builder.Configuration);
 
