fcaa8055b63239637c32edbee85862eb50e3e969
arseniy <arseniyklevts@gmail.com>
2024-06-26 23:07:11 +0300

    add Global Exception Handler

---
+++ WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using OnlineShop.Application.Exceptions;
+using System.Net;
+
+namespace OnlineShop.WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        ExceptionResponse response = exception switch
+        {
+            ApplicationException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Application exception occurred."),
+            KeyNotFoundException _ => new ExceptionResponse(HttpStatusCode.NotFound, "The request key not found."),
+            UnauthorizedAccessException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Unauthorized."),
+            _ => new ExceptionResponse(HttpStatusCode.InternalServerError, "Internal server error. Please retry later.")
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)response.StatusCode;
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
