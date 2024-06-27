f1cdbd079b055965a773d6126508310cd58d4626
ArseniyKlevtsov <127730797+ArseniyKlevtsov@users.noreply.github.com>
2024-06-27 22:09:20 +0300

    add Global Exception Handler (#29) (#37)
    * add Global Exception Handler
    
    * delete default exception

---
+++ WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using OnlineShop.Application.Exceptions;
+using System.Net;
+
+namespace OnlineShop.WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        ExceptionResponse response = exception switch
+        {
+
+            _ => new ExceptionResponse(HttpStatusCode.InternalServerError, "Internal server error. Please retry later.")
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)response.StatusCode;
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
