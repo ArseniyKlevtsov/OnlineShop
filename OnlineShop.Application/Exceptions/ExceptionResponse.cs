fac65c4a911f86ec43f1686b40b4b49b071b7a3c
ArseniyKlevtsov <127730797+ArseniyKlevtsov@users.noreply.github.com>
2024-06-27 21:54:22 +0300

    add Global Exception Handler (#29)
    * add Global Exception Handler
    
    * delete default exception

---
+++ OnlineShop.Application/Exceptions/ExceptionResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace OnlineShop.Application.Exceptions;
+
+public class ExceptionResponse(HttpStatusCode httpStatusCode, string message)
+{
+
+    public string Message { get; set; } = message;
+    public HttpStatusCode StatusCode { get; set; } = httpStatusCode;
+}
