54b982d8bc8b470bdc344b2e5385db2c2f9902b1
Даша Рум <dasharum03@mail.ru>
2024-07-01 04:18:32 +0300

    feature/OrdersController

--- OnlineShop.Application/Mapping/MappingProfile.cs
+++ OnlineShop.Application/Mapping/MappingProfile.cs
@@ -1,14 +1,34 @@
 ﻿using AutoMapper;
-using OnlineShop.Domain.Entities;
 using OnlineShop.Application.DTOs.OrderDTOs.Requests;
 using OnlineShop.Application.DTOs.OrderDTOs.Responses;
+using OnlineShop.Application.DTOs.OrderItemsDTOs.Requests;
+using OnlineShop.Application.DTOs.OrderItemsDTOs.Responses;
+using OnlineShop.Domain.Entities;
 
-namespace OnlineShop.Application.Mapping
+namespace OnlineShop.Application.MappingProfiles
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            CreateMap<CreateOrderItemRequestDto, OrderItem>()
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.OrderId))
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice));
+
+            CreateMap<UpdateOrderItemRequestDto, OrderItem>()
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice));
+
+            CreateMap<OrderItem, OrderItemDtoResponse>()
+                .ForMember(dest => dest.OrderItemId, opt => opt.MapFrom(src => src.OrderItemId))
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.OrderId))
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.ProductName));
+
             CreateMap<Order, OrderResponseDto>()
                 .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.OrderId))
                 .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
